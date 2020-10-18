cfc02b60241e48637fddcfc612ed143252ac1d0a
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2020-10-18 15:47:04 +0200

    Peak Index In a Mountain Array solution added.

---
+++ src/main/java/PeakIndexInAMountainArray/Solution.java
@@ -0,0 +1,15 @@
+package main.java.PeakIndexInAMountainArray;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int i = 0;
+
+        while ((arr[i] < arr[i + 1])) {
+            i++;
+        }
+
+        return i;
+    }
+}
+
+
