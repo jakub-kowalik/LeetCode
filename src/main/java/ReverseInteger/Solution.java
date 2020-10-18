492c5d38a5941151a9cab911dfe909ddd7916e5f
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2020-10-18 22:21:53 +0200

    Reverse Integer solution added.

---
+++ src/main/java/ReverseInteger/Solution.java
@@ -0,0 +1,23 @@
+package main.java.ReverseInteger;
+
+class Solution {
+    public int reverse(int x) {
+        int flag = 0;
+        if (x < 0) {
+            x *= -1;
+            flag = 1;
+        }
+
+        StringBuilder str = new StringBuilder(Integer.toString(x));
+        try {
+            int answer = new Integer(String.valueOf(str.reverse()));
+
+            if (flag == 1)
+                answer *= -1;
+            return answer;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+}
