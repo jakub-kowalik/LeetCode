d4150ce0fc6359eb32594097e730057be724409c
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2020-10-19 15:35:14 +0200

    Third Max solution added. Perfect Number solution added. Count Primes solution added.

---
+++ src/main/java/ThirdMaximumNumber/Solution.java
@@ -0,0 +1,28 @@
+package main.java.ThirdMaximumNumber;
+
+import java.util.Arrays;
+
+class Solution {
+    public int thirdMax(int[] nums) {
+        if (nums.length < 3) {
+            Arrays.sort(nums);
+            return nums[nums.length-1];
+        }
+
+        int answer = 0;
+        int flag = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = nums.length-1; i > 0; i--) {
+            if (nums[i] > nums[i-1]) {
+                answer = nums[i-1];
+                flag++;
+            }
+            if (flag == 2)
+                return answer;
+        }
+
+        return nums[nums.length-1];
+    }
+}
