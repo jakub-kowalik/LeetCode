9676a5a44ce5f038533bcec0fdfa41776bbdadee
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2020-10-20 22:42:08 +0200

    Shuffle String solution added.

---
+++ src/main/java/ShuffleString/Solution.java
@@ -0,0 +1,28 @@
+package main.java.ShuffleString;
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        boolean isSorted = false;
+        char[] chars = s.toCharArray();
+        int tempInt;
+        char tempChar;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < indices.length - 1; i++) {
+                if (indices[i] > indices[i + 1]) {
+                    tempInt = indices[i];
+                    indices[i] = indices[i + 1];
+                    indices[i + 1] = tempInt;
+
+                    tempChar = chars[i];
+                    chars[i] = chars[i + 1];
+                    chars[i + 1] = tempChar;
+                    isSorted = false;
+                }
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+}
