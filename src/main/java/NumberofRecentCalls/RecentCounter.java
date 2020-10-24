90251075e1a5dcc96df86f1d430c0c1d2a053a48
Jakub Kowalik <56927720+jakub-kowalik@users.noreply.github.com>
2020-10-24 20:08:56 +0200

    New solutions added.

--- src/main/java/NumberofRecentCalls/RecentCounter.java
+++ src/main/java/NumberofRecentCalls/RecentCounter.java
@@ -1,4 +1,4 @@
-package main.java.NumberofRecentCalls;
+package main.java.NumberOfRecentCalls;
 
 import java.util.LinkedList;
 import java.util.Queue;
---
+++ src/main/java/XOROperationinanArray/Solution.java
@@ -0,0 +1,14 @@
+package main.java.XOROperationinanArray;
+
+class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = start + 2 * i;
+            answer ^= nums[i];
+        }
+        return answer;
+    }
+}
